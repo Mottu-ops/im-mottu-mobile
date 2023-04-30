d2ed186c3e557ce9ee42d0387b38554ff1d2a146
Gabriel Oliveira <gabrielwillian841@gmail.com>
2023-04-30 10:51:41 -0300

    feat: create splash screen

---
+++ marvel/lib/presentation/splash/view/splash_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:marvel/presentation/splash/view/splash_view.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) => const SplashView();
+}
---
+++ marvel/lib/presentation/splash/view/splash_view.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> sizeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    sizeAnimation = Tween<double>(begin: 200.0, end: 0.4).animate(
+        CurvedAnimation(parent: controller, curve: Curves.easeOutQuart))
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          Future.delayed(const Duration(seconds: 2), () {
+            controller.reverse();
+          });
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
+
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: const Color(0XFFED1D24),
+          child: Transform.scale(
+            scale: sizeAnimation.value,
+            child: SvgPicture.asset(
+              'assets/images/logo_marvel.svg',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
