a8adf8eca90b401d66e7273ba8e0dc809a0f05f6
Gabriel Oliveira <gabrielwillian841@gmail.com>
2023-04-30 16:15:17 -0300

    feat: implementation class Failure

---
+++ marvel/lib/core/erros/failures.dart
@@ -0,0 +1,42 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  const Failure();
+}
+
+class ServerFailure extends Failure {
+  final int? statusCode;
+  final String? type;
+  final String? message;
+  final Exception? exception;
+
+  const ServerFailure({
+    this.statusCode,
+    this.type,
+    this.message,
+    this.exception,
+  });
+
+  @override
+  List<Object?> get props => [statusCode, type, message, exception];
+}
+
+class NetworkInfoFailure extends Failure {
+  final String? message;
+  final Exception? exception;
+
+  const NetworkInfoFailure({this.message, this.exception});
+
+  @override
+  List<Object?> get props => [message, exception];
+}
+
+class CacheFailure extends Failure {
+  final String? message;
+  final Exception? exception;
+
+  const CacheFailure({this.message, this.exception});
+
+  @override
+  List<Object?> get props => [message, exception];
+}
