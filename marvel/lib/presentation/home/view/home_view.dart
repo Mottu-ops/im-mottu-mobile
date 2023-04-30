8155a9b4b17a602f02d7b01344a72aac1c956fab
Gabriel Oliveira <gabrielwillian841@gmail.com>
2023-04-30 16:14:00 -0300

    refactor: adjust interface home_view.dart

--- marvel/lib/presentation/home/view/home_view.dart
+++ marvel/lib/presentation/home/view/home_view.dart
@@ -12,16 +12,83 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            backgroundColor: const Color(0XFF202020),
-            centerTitle: true,
-            title: SvgPicture.asset(
-              'assets/images/logo_marvel_with_background.svg',
+      appBar: AppBar(
+        toolbarHeight: 150,
+        elevation: 0,
+        backgroundColor: Colors.black,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(50),
+          child: Container(
+            margin: const EdgeInsets.only(right: 24, left: 24, bottom: 24),
+            child: Column(
+              children: [
+                SvgPicture.asset(
+                  'assets/images/logo_marvel_with_background.svg',
+                ),
+                const SizedBox(height: 48),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                        child: const TextField(
+                          cursorColor: Colors.black,
+                          style: TextStyle(fontSize: 16),
+                          decoration: InputDecoration(
+                            prefixIconColor: Colors.black38,
+                            prefixIcon: Icon(Icons.search),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    const Icon(
+                      Icons.filter_list_rounded,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ],
             ),
           ),
-        ],
+        ),
+      ),
+      body: NotificationListener(
+        onNotification: (OverscrollIndicatorNotification notification) {
+          notification.disallowIndicator();
+          return true;
+        },
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              children: List.generate(
+                70,
+                (index) => ExpansionTile(
+                  expandedCrossAxisAlignment: CrossAxisAlignment.start,
+                  title: Text(index.toString()),
+                  children: [
+                    GestureDetector(
+                      onTap: () =>
+                          print('Interface detail ${index.toString()}'),
+                      child: Image.network(
+                        'http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784/detail.jpg',
+                      ),
+                    ),
+                    Text('Name ${index.toString()}'),
+                    Text('Description ${index.toString()}'),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
