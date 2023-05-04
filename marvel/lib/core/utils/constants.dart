e4da5dd77e1ccf81d8c33e8f5ffa6a4514b76077
Gabriel Oliveira <gabrielwillian841@gmail.com>
2023-05-03 23:26:07 -0300

    feat: create class Constants

---
+++ marvel/lib/core/utils/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  static const String baseUrl = 'https://gateway.marvel.com/v1/public';
+  static const String apiKey = 'da4c38edd60d9f0b8f318dc06dba1cb1';
+  static const String ts = 'c4ca4238a0b923820dcc509a6f75849b';
+  static const String hash = '232b53d804801e8042a8e2df8a6c0c19';
+
+  static const String montserrat = 'Montserrat';
+  static const String poppins = 'Poppins';
+}
