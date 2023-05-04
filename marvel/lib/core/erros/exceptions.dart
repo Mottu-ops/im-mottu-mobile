7782d4f505acd6aa49a302dd9478b9e5280a5cba
Gabriel Oliveira <gabrielwillian841@gmail.com>
2023-05-03 23:28:50 -0300

    feat: implementation class HttpException

--- marvel/lib/core/erros/exceptions.dart
+++ marvel/lib/core/erros/exceptions.dart
@@ -18,3 +18,12 @@ class NetworkInfoException extends Equatable implements Exception {
   @override
   List<Object?> get props => [message];
 }
+
+class HttpException extends Equatable implements Exception {
+  final String? message;
+
+  const HttpException({this.message});
+
+  @override
+  List<Object?> get props => [message];
+}
