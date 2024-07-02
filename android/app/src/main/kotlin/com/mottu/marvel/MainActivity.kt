0551f317e896e477dc5aad51afeddaffe3c22587
Ismael Pedro <ismael.ramos@contractor-mc1.com>
2024-07-02 20:52:03 -0300

    method channel check connection kotlin + swift

--- android/app/src/main/kotlin/com/mottu/marvel/MainActivity.kt
+++ android/app/src/main/kotlin/com/mottu/marvel/MainActivity.kt
@@ -1,5 +1,62 @@
 package com.mottu.marvel
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "com.mottu.marvel/connectivity"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "startConnectivityListener") {
+                startConnectivityListener()
+                result.success(null)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun startConnectivityListener() {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            connectivityManager.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    runOnUiThread { sendConnectivityMessage("online") }
+                }
+
+                override fun onLost(network: Network) {
+                    runOnUiThread { sendConnectivityMessage("offline") }
+                }
+            })
+        } else {
+            val intentFilter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+            registerReceiver(object : BroadcastReceiver() {
+                override fun onReceive(context: Context, intent: Intent) {
+                    val activeNetwork = connectivityManager.activeNetworkInfo
+                    if (activeNetwork != null && activeNetwork.isConnected) {
+                        sendConnectivityMessage("online")
+                    } else {
+                        sendConnectivityMessage("offline")
+                    }
+                }
+            }, intentFilter)
+        }
+    }
+
+    private fun sendConnectivityMessage(status: String) {
+        MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger ?: return, CHANNEL).invokeMethod("updateConnectivityStatus", status)
+    }
+}
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,58 @@
-import Flutter
 import UIKit
+import Flutter
+import Reachability
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  private let CHANNEL = "com.mottu.marvel/connectivity"
+  var reachability: Reachability?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    if let controller = window?.rootViewController as? FlutterViewController {
+      let connectivityChannel = FlutterMethodChannel(name: CHANNEL, binaryMessenger: controller.binaryMessenger)
+
+      connectivityChannel.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) in
+        if call.method == "startConnectivityListener" {
+          self.startConnectivityListener()
+          result(nil)
+        } else {
+          result(FlutterMethodNotImplemented)
+        }
+      }
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  private func startConnectivityListener() {
+    reachability = try? Reachability()
+
+    reachability?.whenReachable = { reachability in
+      if reachability.connection == .wifi || reachability.connection == .cellular {
+        self.sendConnectivityMessage(status: "online")
+      }
+    }
+
+    reachability?.whenUnreachable = { _ in
+      self.sendConnectivityMessage(status: "offline")
+    }
+
+    do {
+      try reachability?.startNotifier()
+    } catch {
+      print("Unable to start notifier")
+    }
+  }
+
+  private func sendConnectivityMessage(status: String) {
+    if let controller = window?.rootViewController as? FlutterViewController {
+      let connectivityChannel = FlutterMethodChannel(name: CHANNEL, binaryMessenger: controller.binaryMessenger)
+      connectivityChannel.invokeMethod("updateConnectivityStatus", arguments: status)
+    }
   }
 }
