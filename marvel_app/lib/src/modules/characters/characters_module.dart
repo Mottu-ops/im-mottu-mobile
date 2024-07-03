95c4f10d05c3b9a525ec0e415c1576a55e97e9eb
Emilson Silva <emilson.silva@nava.com.br>
2024-07-03 13:09:40 -0300

    Add CharactersResult and CharactersController instances with dependency injection

--- marvel_app/lib/src/modules/characters/characters_module.dart
+++ marvel_app/lib/src/modules/characters/characters_module.dart
@@ -2,19 +2,17 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:marvel_app/src/modules/characters/pages/person/person_module.dart';
 import 'package:marvel_app/src/modules/characters/pages/characters/characters_controller.dart';
 import 'package:marvel_app/src/modules/characters/pages/characters/characters_page.dart';
-import 'package:marvel_app/src/shared/data/characters_model.dart';
-import 'package:marvel_app/src/shared/tools/constants/api_mock_map.dart';
 
 class CharactersModule extends Module {
   @override
   void binds(i) {
-    i.addInstance<CharactersResult>(CharactersResult.fromJson(API_MOCK_MAP));
-    i.addInstance<CharactersController>(CharactersController(result: i()));
+    i.addInstance<CharactersController>(
+        CharactersController(client: Modular.get()));
   }
 
   @override
   void routes(r) {
     r.child('/', child: (_) => const CharactersPage());
-    r.module('/person', module:PersonModule());
+    r.module('/person', module: PersonModule());
   }
 }
