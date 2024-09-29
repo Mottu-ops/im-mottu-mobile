a46ac61e9218bcd533d9b6b0e05a4bcd1caaade2
Thiago Sales <thiagobrunoms@gmail.com>
2024-09-29 11:29:13 -0300

    feat: change app initialization to allow async initiations

--- apps/mottu_marvel/lib/main.dart
+++ apps/mottu_marvel/lib/main.dart
@@ -1,17 +1,35 @@
+import 'package:common/common.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:mottu_marvel/characters/presentation/routes.dart';
 import 'package:mottu_marvel/routes/app_routes.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async => startApplication();
+
+Future<void> startApplication() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  setFlavor();
+  await dotenv.load();
+
+  print('App flavor $appFlavor');
+  print('Environemnt URL ${Environment.baseApiUrl}');
+  await Future.wait([
+    lockRotation(),
+    // initFirebase(),
+  ]);
+
+  runApp(const MottuMarvelApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MottuMarvelApp extends StatelessWidget {
+  const MottuMarvelApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //ADD INITIALIZATION PROCESS TO .env
+
     return GetMaterialApp(
       title: 'Mottu - Marvel',
       theme: ThemeData(
