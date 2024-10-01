0eda4f00b29d09a53ce98ce5387203c953abe42a
Thiago Sales <thiagobrunoms@gmail.com>
2024-10-01 05:38:13 -0300

    feat: create a core layer to include basic use cases, services, routes and other reusable features for the app

---
+++ apps/mottu_marvel/lib/core/routes/app_routes.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+import 'package:mottu_marvel/modules/characters/presentation/routes.dart';
+
+final marvelPages = <GetPage<dynamic>>[
+  ...CharactersPagesRoutes.routes,
+];
