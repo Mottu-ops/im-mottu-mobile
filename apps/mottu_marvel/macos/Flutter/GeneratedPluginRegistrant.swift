ae57adf10935ff357f313cfe66b20ae3233d7b44
Thiago Sales <thiagobrunoms@gmail.com>
2024-10-02 23:06:07 -0300

    feat-build: add firebase and firebase crashlytics package and configs

--- apps/mottu_marvel/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/mottu_marvel/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
+import firebase_crashlytics
 import package_info_plus
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
