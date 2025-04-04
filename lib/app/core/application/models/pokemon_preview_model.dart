6e36598716fd53486c79f822f2f2f67da02250c4
João Pedro <devjpdesenvolver@gmail.com>
2025-04-04 10:36:12 -0300

    feat: formated id and name in PokemonPreviewModel

---
+++ lib/app/core/application/models/pokemon_preview_model.dart
@@ -0,0 +1,27 @@
+class PokemonPreviewModel {
+  final String name;
+  final String imageUrl;
+  final int id;
+
+  PokemonPreviewModel(
+      {required this.name, required this.imageUrl, required this.id});
+
+  String get formattedName {
+    return name[0].toUpperCase() + name.substring(1);
+  }
+
+  String get formattedId {
+    String idString = id.toString();
+    return '#${'0' * (3 - idString.length)}$idString';
+  }
+
+  factory PokemonPreviewModel.fromJson(Map<String, dynamic> map) {
+    int id = int.parse(map['url'].split('/').reversed.elementAt(1));
+    return PokemonPreviewModel(
+      name: map['name'],
+      id: id,
+      imageUrl:
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$id.png',
+    );
+  }
+}
