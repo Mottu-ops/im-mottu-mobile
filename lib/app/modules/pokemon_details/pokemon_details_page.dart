c4382ce3c00a8497fabec9c2e4ae820d7246db61
João Pedro <devjpdesenvolver@gmail.com>
2025-04-04 17:35:58 -0300

    feat: create pokemon details page

---
+++ lib/app/modules/pokemon_details/pokemon_details_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PokemonDetailsPage extends StatelessWidget {
+  const PokemonDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
