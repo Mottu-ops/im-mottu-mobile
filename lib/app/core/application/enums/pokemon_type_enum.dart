cf3697afe60eb67cec4e561e0452c0e53120c1d5
João Pedro <devjpdesenvolver@gmail.com>
2025-04-06 16:21:20 -0300

    feat: add icons and name in types

--- lib/app/core/application/enums/pokemon_type_enum.dart
+++ lib/app/core/application/enums/pokemon_type_enum.dart
@@ -3,29 +3,32 @@ import 'dart:ui';
 import 'package:pokedex/app/core/application/theme/palettes.dart';
 
 enum PokemonTypeEnum {
-  normal(Palettes.normalColor),
-  fighting(Palettes.fightingColor),
-  flying(Palettes.flyingColor),
-  poison(Palettes.poisonColor),
-  ground(Palettes.groundColor),
-  rock(Palettes.rockColor),
-  bug(Palettes.bugColor),
-  ghost(Palettes.ghostColor),
-  steel(Palettes.steelColor),
-  fire(Palettes.fireColor),
-  water(Palettes.waterColor),
-  grass(Palettes.grassColor),
-  electric(Palettes.electricColor),
-  psychic(Palettes.psychicColor),
-  ice(Palettes.iceColor),
-  dragon(Palettes.dragonColor),
-  dark(Palettes.darkColor),
-  fairy(Palettes.fairyColor),
-  stellar(Palettes.stellarColor),
-  unknown(Palettes.unknownColor);
+  normal(Palettes.normalColor, 'assets/icons/normal_icon.svg', 'Normal'),
+  fighting(Palettes.fightingColor, 'assets/icons/fighting_icon.svg', 'Lutador'),
+  flying(Palettes.flyingColor, 'assets/icons/flying_icon.svg', 'Voador'),
+  poison(Palettes.poisonColor, 'assets/icons/poison_icon.svg', 'Venenoso'),
+  ground(Palettes.groundColor, 'assets/icons/ground_icon.svg', 'Terra'),
+  rock(Palettes.rockColor, 'assets/icons/rock_icon.svg', 'Pedra'),
+  bug(Palettes.bugColor, 'assets/icons/bug_icon.svg', 'Inseto'),
+  ghost(Palettes.ghostColor, 'assets/icons/ghost_icon.svg', 'Fantasma'),
+  steel(Palettes.steelColor, 'assets/icons/steel_icon.svg', 'Aço'),
+  fire(Palettes.fireColor, 'assets/icons/fire_icon.svg', 'Fogo'),
+  water(Palettes.waterColor, 'assets/icons/water_icon.svg', 'Água'),
+  grass(Palettes.grassColor, 'assets/icons/grass_icon.svg', 'Planta'),
+  electric(
+      Palettes.electricColor, 'assets/icons/electric_icon.svg', 'Elétrico'),
+  psychic(Palettes.psychicColor, 'assets/icons/psychic_icon.svg', 'Psíquico'),
+  ice(Palettes.iceColor, 'assets/icons/ice_icon.svg', 'Gelo'),
+  dragon(Palettes.dragonColor, 'assets/icons/dragon_icon.svg', 'Dragão'),
+  dark(Palettes.darkColor, 'assets/icons/dark_icon.svg', 'Sombrio'),
+  fairy(Palettes.fairyColor, 'assets/icons/fairy_icon.svg', 'Fada'),
+  stellar(Palettes.stellarColor, 'assets/icons/fire_icon.svg', 'Fogo'),
+  unknown(Palettes.unknownColor, null, 'Desconhecido');
 
   final Color color;
-  const PokemonTypeEnum(this.color);
+  final String name;
+  final String? iconPath;
+  const PokemonTypeEnum(this.color, this.iconPath, this.name);
 
   static PokemonTypeEnum fromJson(Map<String, dynamic> json) {
     return PokemonTypeEnum.values.firstWhere(
